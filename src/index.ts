0b1dcad02f17830abc20e05237984336af7e452b
Kim Astrom <kim.astrom@gmail.com>
2018-11-30 14:19:47 +0100

    merge

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import NimbleProvider from './lib/components/Provider'
-import View from './lib/components/View'
-import { IViewProps } from './lib/types/view'
-import { ITextProps } from './lib/types/text'
+import NimbleProvider from "./lib/components/Provider"
+import View from "./lib/components/View"
+import { IViewProps } from "./lib/types/view"
+import { ITextProps } from "./lib/types/text"
 
 export { View, NimbleProvider, IViewProps, ITextProps }
--- src/lib/components/View.tsx
+++ src/lib/components/View.tsx
@@ -1,12 +1,12 @@
-import * as React from 'react'
-import { View as OView, StyleSheet } from 'react-native'
-import { IViewProps } from '../types/view';
-import { viewPropToStylePropMap } from '../utils/maps/view';
-import NimbleConsumer from './Consumer';
-import { IConsumerInjectedProps } from '../types/consumer';
-import { generalPropToStylePropMap } from '../utils/maps/general';
-import { getViewUtiityStyles } from '../utils/getUtilityStyles';
-import getRegularStyles from '../utils/getRegularStyles';
+import * as React from "react"
+import { View as OView, StyleSheet } from "react-native"
+import { IViewProps } from "../types/view"
+import { viewPropToStylePropMap } from "../utils/maps/view"
+import NimbleConsumer from "./Consumer"
+import { IConsumerInjectedProps } from "../types/consumer"
+import { generalPropToStylePropMap } from "../utils/maps/general"
+import { getViewUtiityStyles } from "../utils/getUtilityStyles"
+import getRegularStyles from "../utils/getRegularStyles"
 
 const View: React.SFC<IViewProps & IConsumerInjectedProps> = ({
   children,
@@ -14,22 +14,21 @@ const View: React.SFC<IViewProps & IConsumerInjectedProps> = ({
   theme,
   ...rest
 }) => {
-  const propToStylePropMap = new Map([...viewPropToStylePropMap, ...generalPropToStylePropMap])
+  const propToStylePropMap = new Map([
+    ...viewPropToStylePropMap,
+    ...generalPropToStylePropMap
+  ])
   const regularStyles = getRegularStyles(rest, propToStylePropMap, theme)
   const utilityStyles = getViewUtiityStyles(rest)
   const appliedStyle = StyleSheet.create({
     style: {
       ...regularStyles,
       ...style,
-      ...utilityStyles,
+      ...utilityStyles
     }
   })
 
-  return (
-    <OView style={ appliedStyle.style }>
-      { children }
-    </OView>
-  )
+  return <OView style={appliedStyle.style}>{children}</OView>
 }
 
 export default NimbleConsumer(View)
