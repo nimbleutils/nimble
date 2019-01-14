da9fdab153c048094e70053db20a4f51bcd5da08
Oliwer Smedjevik <oliwer.smedjevik@gmail.com>
2019-01-14 08:08:27 +0100

    -..-

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  title: "Nimble-utils",
-  description: "Probably the best design utility package in the world.",
-  head: [["link", { rel: "icon", href: `/logo.png` }]],
-  base: "/",
-  dest: "./dist",
+  title: 'Nimble-utils',
+  description: 'Probably the best design utility package in the world.',
+  head: [['link', { rel: 'icon', href: `/logo.png` }]],
+  base: '/',
+  dest: './dist',
 
   themeConfig: {
     nav: [
-      { text: "Home", link: "/" },
-      { text: "Guide", link: "/guide/" },
-      { text: "News", link: "/news/" },
-      { text: "GitHub ⤴", link: "https://github.com/goa-codebet/nimble-utils" }
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'News', link: '/news/' },
+      { text: 'GitHub ⤴', link: 'https://github.com/goa-codebet/nimble-utils' },
     ],
     sidebar: {
-      '/guide/': genSidebarConfig('Guide')
+      '/guide/': genSidebarConfig('Guide'),
     },
-    lastUpdated: 'Last Updated'
+    lastUpdated: 'Last Updated',
   },
 
   serviceWorker: true,
@@ -24,24 +24,19 @@ module.exports = {
     // options for markdown-it-anchor
     anchor: { permalink: false },
     config: md => {
-      md.use(require("markdown-it-katex"));
-    }
-  }
-};
+      md.use(require('markdown-it-katex'))
+    },
+  },
 
-function genSidebarConfig (title) {
+  plugins: [require('./highlightjs')],
+}
+
+function genSidebarConfig(title) {
   return [
     {
       title,
       collapsable: true,
-      children: [
-        '',
-        'getting-started',
-        'components',
-        'advanced',
-        'about',
-      ]
-    }
+      children: ['', 'getting-started', 'components', 'advanced', 'about'],
+    },
   ]
 }
-
