44956ac565a4358b4957c64a480da0ba8271a829
Kim Astrom <kim.astrom@gmail.com>
2019-02-08 14:50:22 +0100

    click directive 50% implemented

--- src/lib/types/directives.ts
+++ src/lib/types/directives.ts
@@ -1,3 +1,12 @@
 export interface IGeneralDirectives {
   nIf?: boolean
+  nClick?: IClickDirectives
 }
+
+type touchables =
+  | 'TouchableHighlight'
+  | 'TouchableOpacity'
+  | 'TouchableNativeFeedback'
+  | 'TouchableWithoutFeedback'
+
+export type IClickDirectives = Function | [touchables, Function]
--- src/lib/utils/directives.tsx
+++ src/lib/utils/directives.tsx
@@ -1,9 +1,40 @@
+import React, { Component } from 'react'
+import { isFunction } from 'lodash'
+import { TouchableOpacity } from 'react-native'
+import { IClickDirectives } from '../types/directives'
+
 export const ifDirective = (c: any, nIf: boolean) => {
   return nIf || typeof nIf === 'undefined' ? c : null
 }
 
-export const renderWithDirectives = (element, { nIf }: { nIf?: boolean }) => {
+export const clickDirective = (c: Component, nClick: any) => {
+  if (isFunction(nClick)) {
+    return <TouchableOpacity onPress={() => nClick}>{c}</TouchableOpacity>
+  }
+
+  if (Array.isArray(nClick) && nClick.length > 1) {
+    return React.createElement(nClick[0], { onPress: nClick[1] }, c)
+  }
+
+  return null
+}
+
+export const renderWithDirectives = (
+  element,
+  { nIf, nClick }: { nIf?: boolean; nClick?: IClickDirectives }
+) => {
   let modifiedElement = element
+
+  // If directive
   modifiedElement = ifDirective(modifiedElement, nIf)
+  if (!modifiedElement) return modifiedElement
+
+  if (nClick) {
+    console.log(nClick)
+  }
+
+  Wrapp in click
+  modifiedElement = clickDirective(modifiedElement, nClick)
+
   return modifiedElement
 }
